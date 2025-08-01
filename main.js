c6c4a2fa437e50f7e629d3f4646be68678f9ae25
Stephany Millena <137302762+fanyeverson@users.noreply.github.com>
2025-08-01 18:55:16 -0300

    net

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%#*?';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 function diminuiTamanho() {
     if (tamanhoSenha > 1) {
-        /*tamanhoSenha = tamanhoSenha-1;*/
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
@@ -20,15 +25,9 @@ function aumentaTamanho() {
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
-const campoSenha = document.querySelector('#campo-senha');
-const checkbox = document.querySelectorAll('.checkbox');
 for (i = 0; i < checkbox.length; i++) {
     checkbox[i].onclick = geraSenha;
 }
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
-const numeros = '0123456789';
-const simbolos = '!@%#*?';
 geraSenha();
 function geraSenha() {
     let alfabeto = '';
@@ -44,8 +43,8 @@ function geraSenha() {
     if (checkbox[3].checked) {
         alfabeto = alfabeto + simbolos;
     }
-
     let senha = '';
+
     for (let i = 0; i < tamanhoSenha; i++) {
         let numeroAleatorio = Math.random() * alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
@@ -53,9 +52,4 @@ function geraSenha() {
     }
     campoSenha.value = senha;
     classificaSenha();
-
-}
-const forcaSenha = document.querySelector('.forca');
-function classificaSenha() {
-    forcaSenha.classList.add('forte');
 }
